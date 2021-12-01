4e50a32850c13209c831a39b5ad2cbfd51476737
Arnaud Agbo <arnaudagbo@macbook-pro-de-arnaud.home>
2021-12-01 10:14:12 +0100

    routes sauces et authentification login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://arnaud:Aarnowlin75015!@cluster0.t6itt.mongodb.net/Cluster0?retryWrites=true&w=majority',
@@ -23,6 +24,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());  
 
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app; 
